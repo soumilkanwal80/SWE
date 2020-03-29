b0673dd1888b53404419014de961588074cbf022
OEM Configuration (temporary user) <abhinavkhanna101@gmail.com>
2020-03-29 18:35:50 +0530

    css loads correctly

--- main.py
+++ main.py
@@ -16,16 +16,24 @@
 # eel.start('home.html', size=(1000, 600))
 
 from flask import Flask,render_template
+import easygui
+import cv2
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
+@app.route('/home.html')
 def home_page():
     return render_template('home.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')   
+@app.route('/faces')
+@app.route('/faces.html')
+def faces():
+    return render_template('faces.html')
 
 
 if __name__ == '__main__':
