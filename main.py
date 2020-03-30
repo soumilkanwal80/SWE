ff5f7cbf8c0bc0ac9ebeaf3f963bd3a18e794de6
saurav8683 <37307840+saurav8683@users.noreply.github.com>
2020-03-30 11:02:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,50 @@ def select_target():
 	cv2.destroyAllWindows()
 	return render_template('faces.html')    
 
+@app.route('/input')
+@app.route('/input.html')
+def input():
+    return render_template('input.html')
+
+@app.route('/')
+@app.route('/select_input')
+def select_input():
+	cpt = sum([len(files) for r, d, files in os.walk("target")])
+
+
+	file = easygui.fileopenbox()
+	frame = cv2.imread(file)
+	
+	number=cpt+1
+	filename=str(number)+'.jpg'
+	path = './target'
+	cv2.imwrite(os.path.join(path , filename), frame)
+	cv2.destroyAllWindows()
+	return render_template('input.html')
+
+@app.route('/workflow')
+@app.route('/workflow.html')
+def workflow():
+    return render_template('workflow.html')
+
+@app.route('/')
+@app.route('/run_workflow')
+def run_workflow():
+	person=[]
+	folder="images"
+	for filename in os.listdir(folder):
+	    img = cv2.imread(os.path.join(folder,filename))
+	    img= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	    if img is not None:
+	        person.append(img)
+	target=[]
+	folder="target"
+	for filename in os.listdir(folder):
+	    img = cv2.imread(os.path.join(folder,filename))
+	    img= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	    if img is not None:
+	        target.append(img)
+	return render_template('workflow.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)    
