3490299ed98bfdec9d38b636ca17e801224ac289
OEM Configuration (temporary user) <abhinavkhanna101@gmail.com>
2020-04-01 16:19:57 +0530

    Cache disabled checked

--- main.py
+++ main.py
@@ -201,5 +201,19 @@ def run_workflow():
 def test_css():
 	return render_template('test.html')	
 
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 if __name__ == '__main__':
 	app.run(debug=True)    
