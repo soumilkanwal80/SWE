8eb94179ec19c6e7175417a6f940955d0c463261
saurav8683 <37307840+saurav8683@users.noreply.github.com>
2020-04-09 12:34:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -103,6 +103,39 @@ def select_input():
 	cv2.destroyAllWindows()
 	return render_template('input.html', number = number, flag = 1)
 
+@app.route('/')
+@app.route('/select_input_video')
+def select_input_video():
+	cpt = sum([len(files) for r, d, files in os.walk("static/target_video")])
+
+
+	file = easygui.fileopenbox()
+	if file == None:
+		# flash('No image selected')
+		return render_template('input.html', number = cpt, flag = 0)
+
+	if file.lower().endswith(('.mp4', '.mkv')) == False:
+		return render_template('input.html', number = cpt, flag = -1)		
+	
+	cap = cv2.VideoCapture(file)
+	frame_width = int(cap.get(3))
+	frame_height = int(cap.get(4))
+	
+	number=cpt+1
+	filename=str(number)+'.mp4'
+	path = './static/target'
+	out = cv2.VideoWriter('./static/target_video/output.avi',cv2.VideoWriter_fourcc('M','J','P','G'),60, (frame_width,frame_height))
+	while(True):
+		ret, frame = cap.read()
+		if ret==True:
+	  		out.write(frame)
+	  		if cv2.waitKey(1) & 0xFF == ord('q'):
+	  			break
+		else:
+			break 
+	cv2.destroyAllWindows()
+	return render_template('input.html', number = number, flag = 1)
+
 @app.route('/workflow')
 @app.route('/workflow.html')
 def workflow():
@@ -144,6 +177,7 @@ def faceRecognitionImage(targetImage, personImage):
         if True in matches:
             return True
     return False
+
 @app.route('/')
 @app.route('/run_workflow')
 @app.route('/run_workflow/<number>')
@@ -213,6 +247,77 @@ def run_workflow():
 	else:
 		return render_template('workflow2.html')
 
+@app.route('/')
+@app.route('/faceRecognitionVideo')
+def faceRecognitionVideo(targetVideo, personImage):
+    personEncoding = generateEncodingsSingle(personImage)
+    while targetVideo.isOpened():
+        ret, frame = targetVideo.read()
+        if not ret:
+            return "Error"
+
+        target = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        targetEncodings = generateEncodingsSingle(target)
+        for encoding in targetEncodings:
+            matches = face_recognition.compare_faces(personEncoding, encoding)
+            if True in matches:
+                return True
+    return False
+
+
+@app.route('/')
+@app.route('/run_workflow_video')
+@app.route('/run_workflow_video/<number>')
+def run_workflow_video():
+	person=[]
+	folder="static/images"
+	for filename in os.listdir(folder):
+	    img = cv2.imread(os.path.join(folder,filename))
+	    #img= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	    if img is not None:
+	        person.append(img)
+	folder="static/target_video"
+	targetVideo=cv2.VideoCapture("./static/target_video/output.avi")
+	flag=0
+	folder = 'static/detected_video'
+	for filename in os.listdir(folder):
+	    file_path = os.path.join(folder, filename)
+	    try:
+	        if os.path.isfile(file_path) or os.path.islink(file_path):
+	            os.unlink(file_path)
+	        elif os.path.isdir(file_path):
+	            shutil.rmtree(file_path)
+	    except Exception as e:
+        	print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+	folder = 'static/images'
+	for filename in os.listdir(folder):
+	    file_path = os.path.join(folder, filename)
+	    try:
+	        if os.path.isfile(file_path) or os.path.islink(file_path):
+	            os.unlink(file_path)
+	        elif os.path.isdir(file_path):
+	            shutil.rmtree(file_path)
+	    except Exception as e:
+        	print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+	
+
+	if person is not None and targetVideo is not None:
+		cpt=0
+		for i in person:
+			if(faceRecognitionVideo(targetVideo, i)):
+				cpt = sum([len(files) for r, d, files in os.walk("static/detected_video")])
+				number=cpt+1
+				filename=str(number)+'.jpg'
+				path = './static/detected_video'
+				cv2.imwrite(os.path.join(path , filename), i)
+				flag=1		
+
+	if(flag):
+		return render_template('workflow3.html', number = number)
+	else:
+		return render_template('workflow2.html')
 
 @app.route('/test.html')
 def test_css():
